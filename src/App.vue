63426f6799bbe131dc8d67ea8df1dfcfd7efe7d9
AMagicPear <AMagicPear@outlook.com>
2024-09-27 00:45:00 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-  <!-- html -->
-  <div class="app">
-    <h1>你好啊</h1>
-  </div>
+  <Person/>
 </template>
 
 <script lang="ts">
+import Person from './components/Person.vue';
 // JS或TS
 export default {
-  name: 'App'  //组件名
+  name: 'App',  //组件名
+  components:{Person} //注册组件
 }
 </script>
-
-<style>
-/* 样式 */
-.app {
-  background-color: #ddd;
-  box-shadow: 0 0 10px;
-  border-radius: 10px;
-  padding: 20px;
-}
-</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <h2>地址：{{ address }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person">
+import { ref } from 'vue'
+let name = ref('张三')
+let age = ref(18)
+let tel = '13888888888'
+let address = '南京邮电大学'
+function changeName() {
+    name.value = 'zhang-san'
+}
+function changeAge() {
+    age.value += 1
+}
+function showTel() {
+    alert(tel)
+}
+
+</script>
+
+<style scoped>
+.person {
+    background-color: aqua;
+}
+
+button {
+    margin: 0 50px;
+}
+</style>
