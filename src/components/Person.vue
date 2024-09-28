b11140d29a5e3398b8af793b374bf2eec4636cbf
AMagicPear <AMagicPear@outlook.com>
2024-09-28 15:39:56 +0800

    computed

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,48 +1,30 @@
 <template>
-    <div class="person">
-        <h2>一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-        <button @click="changeBrand">修改汽车的品牌</button>
-        <button @click="changePrice">修改汽车的价格</button>
-        <button @click="changeCar">修改汽车</button>
-    </div>
-    <h2>当前求和为：{{ sum }}</h2>
-    <button @click="changeSum">点我sum+1</button>
+  <div class="person">
+    姓：<input type="text" v-model="lastName"><br>
+    名：<input type="text" v-model="firstName"><br>
+    全名：<span>{{ fullName }}</span>
+    全名：<input type="text" :value="fullName"><br>
+  </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, reactive } from 'vue'
-//数据
-let car = reactive({ brand: '奔驰', price: 100 })
-let sum = ref(0)
-let isBenchi = 1;
-function changePrice() {
-    car.price += 10
-}
-function changeSum(){
-    sum.value +=1
-}
-function changeBrand(){
-    console.log(isBenchi);
-    isBenchi = -isBenchi;
-    console.log(isBenchi);
-    car.brand = isBenchi>0?'奔驰':'宝马'
-    console.log(car.brand);
-}
+import {computed, ref} from 'vue'
 
-function changeCar(){
-    // car = {brand:'aaa',price:1}
-    Object.assign(car,{brand:'aaa',price:1})
-}
-
+let lastName = ref("zhang")
+let firstName = ref("san")
+//计算属性，只读
+let fullName = computed(() => {
+  return lastName.value.slice(0,1).toUpperCase()+lastName.value.slice(1)+firstName.value;
+})
 </script>
 
 <style scoped>
 .person {
-    background-color: rgb(226, 241, 225);
-    border-radius: 10px;
+  background-color: rgb(226, 241, 225);
+  border-radius: 10px;
 }
 
 button {
-    margin: 0 50px;
+  margin: 0 50px;
 }
 </style>
