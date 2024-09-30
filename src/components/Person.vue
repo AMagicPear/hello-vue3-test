eb338a0e0704cbc0ab0151d0096b4a7268351257
AMagicPear <AMagicPear@outlook.com>
2024-09-30 20:43:02 +0800

    watch+

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,45 +1,42 @@
 <template>
   <div class="person">
-    <h1>情况一：监视ref定义的基本类型数据</h1>
-    <h2>当前求和为：{{ sum }}</h2>
-    <button @click="changeSum">点我sum+1</button>
-    <h1>情况二：监视ref定义的对象类型数据</h1>
     <h2>姓名：{{ person.name }}</h2>
     <h2>年龄：{{ person.age }}</h2>
+    <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
-    <button @click="changePerson">修改人</button>
+    <button @click="changeC1">修改第一台车</button>
+    <button @click="changeC2">修改第二台车</button>
+    <button @click="changeCar">修改整个车</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, watch } from 'vue'
-let sum = ref(0)
-let person = ref({
-  name: '张三',
-  age: 18
+import { reactive } from 'vue';
+let person = reactive({
+  name: "张三",
+  age: 18,
+  car: {
+    c1: '小米SU7',
+    c2: '五菱宏光小煤球'
+  }
 })
-function changeSum() {
-  sum.value += 1
-}
-const stopWatchSum = watch(sum, (newValue, oldValue) => {
-  console.log("sum变化了", newValue, oldValue)
-  if (newValue >= 10) stopWatchSum();
-})
-
+//方法
 function changeName() {
-  person.value.name += '~'
+  person.name += '~';
 }
 function changeAge() {
-  person.value.age += 1
+  person.age += 1;
 }
-function changePerson() {
-  person.value = { name: '李四', age: 90 }
+function changeC1() {
+  person.car.c1 = "奥迪";
 }
-//监视情况一：监视的是对象的地址值
-watch(person, (newValue, oldValue) => {
-  console.log('person变化了', newValue, oldValue)
-}, { deep: true, immediate: true })
+function changeC2() {
+  person.car.c2 = '大众';
+}
+function changeCar() {
+  person.car = { c1: '雅迪', c2: '爱玛' }
+}
 </script>
 
 <style scoped>
