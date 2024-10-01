2a4f22830c19a184f608d6f7151f5d10cd4ace09
AMagicPear <AMagicPear@outlook.com>
2024-10-01 13:50:47 +0800

    HTML-ref

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,38 +1,18 @@
 <template>
   <div class="person">
-    <h2>需求，当水温达到60度，或水位达到80cm时，给服务器发请求</h2>
-    <h2>当前水温为：{{ temp }}℃</h2>
-    <h2>当前水位为：{{ level }}cm</h2>
-    <button @click="changeTemp">水温+10</button>
-    <button @click="changeLevel">水位+10</button>
+    <h1>中国</h1>
+    <h2 ref="beijing">北京</h2>
+    <h3>尚硅谷</h3>
+    <button @click="showLog">点我输出h2这个元素</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, watch, watchEffect } from 'vue'
-let temp = ref(10)
-let level = ref(0)
-function changeTemp() {
-  temp.value += 10;
+import {ref} from 'vue'
+let beijing = ref(null)
+function showLog(){
+  console.log(beijing.value)
 }
-function changeLevel() {
-  level.value += 10;
-}
-
-//监视
-// watch([temp,level],(value)=>{
-//   let [newTemp,newLevel] = value
-//   if(newTemp>=60||newLevel>=80){
-//     console.log('给服务器发请求')
-//   }
-// })
-
-watchEffect(() => {
-  if(temp.value>=60||level.value>=80){
-    console.log('给服务器发请求')
-  }
-})
-
 </script>
 
 <style scoped>
