0c70c55db269a78fe244ecc93d3e05fb3e9b8351
AMagicPear <AMagicPear@outlook.com>
2024-11-14 00:16:10 +0800

    修正路由

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hello-vue3-test/',
   plugins: [
     vue(),
     vueSetupExtend()
